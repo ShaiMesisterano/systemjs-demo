4f6674b665a34a8abc7c888f86d28a7da224148e
Shai Mesisterano <shai.mesisterano@array.com>
2022-04-28 11:35:20 +0300

    add an example

---
+++ src/foo.js
@@ -0,0 +1,14 @@
+System.register([], function (exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    function foo() {
+        return 'foo';
+    }
+    exports_1("default", foo);
+    return {
+        setters: [],
+        execute: function () {
+        }
+    };
+});
+//# sourceMappingURL=foo.js.map
