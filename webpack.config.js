800f6de15cbdd15563e5ba025a3f394fdb03b2c2
ShaiMesisterano <shai@mcterano.com>
2022-06-19 16:05:23 +0300

    change eslint config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
-const path = require('path');
+const path = require("path");
 const PrettierPlugin = require("prettier-webpack-plugin");
-const ESLintPlugin = require('eslint-webpack-plugin');
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+const commonConfig = {
+    mode: "production",
+    plugins: [
+        new PrettierPlugin({}),
+        new ESLintPlugin({})
+    ],
+}
 
 const generateConfig = ({folder, file}) => ({
+    ...commonConfig,
     entry: path.join(__dirname, "src", folder, `${file}.tsx`),
     output: {
         path:path.resolve(__dirname, "src", folder),
@@ -14,23 +23,20 @@ const generateConfig = ({folder, file}) => ({
                 test: /\.?tsx/,
                 use: [
                     {
-                        loader: 'ts-loader'
+                        loader: "ts-loader"
                     }
                 ]
             },
         ]
     },
-    plugins: [
-        new PrettierPlugin({}),
-        new ESLintPlugin({})
-    ],
 });
 
 const indexConfig = {
+    mode: "production",
     entry: ["@babel/polyfill", path.join(__dirname, "src", "script.js")],
     output: {
         path:path.resolve(__dirname, "src"),
-        filename: `script.build.js`
+        filename: "script.build.js"
     },
 };
 
